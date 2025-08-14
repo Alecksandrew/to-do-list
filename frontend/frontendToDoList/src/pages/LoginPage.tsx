9db9d424543ce5e0b84c5497f398dd03ba764756
Alecksandrew <alecksandrewcontato@gmail.com>
2025-08-14 13:55:33 -0300

    text center login page

--- frontend/frontendToDoList/src/pages/LoginPage.tsx
+++ frontend/frontendToDoList/src/pages/LoginPage.tsx
@@ -5,7 +5,7 @@ export default function LoginPage(){
 
     return (
         <div className="min-h-screen min-w-screen flex flex-col justify-center items-center p-6">
-            <h1 className="mb-10">Login and access your to do list!</h1>
+            <h1 className="mb-10 text-center">Login and access your to do list!</h1>
             <LoginForm className="min-w-2xs w-9/10 max-w-xl"/>
         </div>
 
